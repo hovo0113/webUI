1a4c79b475ae2a4ac23291e160b1a215e5cce742
Hovo <hovo.h96@gmail.com>
2019-04-28 12:34:51 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './root/app.component';
+import {MatCardModule, MatListModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-UI';
+  private items: Array<any> = [
+    {title: 'Contract Compliance', percent: '80%', bgColor: '#9ACECC', percentColor: '#4C6586'},
+    {title: 'Savings', percent: '30%', bgColor: '#7EAD9C'},
+    {title: 'Cost Reductiions', percent: '60%', bgColor: '#2B79A4'},
+    {title: 'Spend Coverage', percent: '70%', bgColor: '#4C6586'},
+    {title: 'Invoice Compliance', percent: '55%', bgColor: '#0E3D57'},
+  ];
 }
